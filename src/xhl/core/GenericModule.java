74ee447ca14ac81fd0a6758553d06692fd761687
Sergej Chodarev <sergej.chodarev@tuke.sk>
2010-03-31 13:06:20 +0200

    Refactor GenericModule

--- src/xhl/core/GenericModule.java
+++ src/xhl/core/GenericModule.java
@@ -1,18 +1,22 @@
 package xhl.core;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 import java.lang.reflect.Array;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 import xhl.core.exceptions.EvaluationException;
 
-public class GenericModule implements Module {
+/**
+ * Base class for implementing new modules.
+ *
+ * Allows to implement module functions as Java methods marked with annotation
+ * <code>&#64;Function</code>.
+ *
+ * @author Sergej Chodarev
+ */
+public abstract class GenericModule implements Module {
 
     protected final Evaluator evaluator;
 
@@ -25,45 +29,57 @@ public class GenericModule implements Module {
         SymbolTable table = new SymbolTable();
         Method[] methods = this.getClass().getDeclaredMethods();
         for (Method method : methods) {
-            Function fann = method.getAnnotation(Function.class);
-            if (fann != null) {
-                String name;
-                if (fann.name().equals(""))
-                    name = method.getName();
-                else
-                    name = fann.name();
-                Executable exec = new GenericExecutable(this, method, fann
-                        .evaluateArgs(), evaluator);
-                table.put(new Symbol(name), exec);
-                System.out.println("+ " + name);
-            }
+            tryAddFunction(table, method);
         }
         return table;
     }
 
+    /**
+     * If method has <code>&#64;Function</code> annotation, add it to symbols
+     * table.
+     *
+     * @param table
+     * @param method
+     */
+    private void tryAddFunction(SymbolTable table, Method method) {
+        Function fann = method.getAnnotation(Function.class);
+        if (fann != null) {
+            // Find function name
+            String name;
+            if (fann.name().equals(""))
+                name = method.getName();
+            else
+                name = fann.name();
+
+            Executable exec = new GenericExecutable(method, fann.evaluateArgs());
+            table.put(new Symbol(name), exec);
+        }
+    }
+
+    /**
+     * Indicates that method should be exported as module function.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.METHOD)
     public @interface Function {
+        /** Function name. If not specified, method name is uses. */
         public String name() default "";
+
         public boolean evaluateArgs() default true;
     }
 
-    public static class GenericExecutable implements Executable {
-        private final GenericModule object;
+    private class GenericExecutable implements Executable {
         private final Method method;
-        private final Evaluator evaluator;
         private final boolean evaluateArgs;
 
-        public GenericExecutable(GenericModule object, Method method,
-                boolean evaluateArgs, Evaluator evaluator) {
-            this.object = object;
+        public GenericExecutable(Method method, boolean evaluateArgs) {
             this.method = method;
             this.evaluateArgs = evaluateArgs;
-            this.evaluator = evaluator;
         }
 
         @Override
         public Object exec(CodeList args) throws EvaluationException {
+            // Prepare arguments
             List<Object> evArgs = new ArrayList<Object>(args.size());
             for (Object arg : args) {
                 if (evaluateArgs)
@@ -71,26 +87,21 @@ public class GenericModule implements Module {
                 else
                     evArgs.add(arg);
             }
+            if (method.isVarArgs())
+                packVarArgs(evArgs);
+
             try {
-                if (method.isVarArgs())
-                    packVarArgs(evArgs);
-                return method.invoke(object, evArgs.toArray());
-            } catch (IllegalArgumentException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (SecurityException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                return method.invoke(GenericModule.this, evArgs.toArray());
+            } catch (Exception e) {
+                throw new EvaluationException();
             }
-            return null;
         }
 
+        /**
+         * Pack arguments to array for method with variable number of arguments.
+         *
+         * @param evArgs
+         */
         private void packVarArgs(List<Object> evArgs) {
             Class<?>[] params = method.getParameterTypes();
             int last = params.length - 1; // index of varargs parameter
