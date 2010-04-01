48d9dcdaaf7e9dc9456e2f176f7e252b74441cb1
Sergej Chodarev <sergej.chodarev@tuke.sk>
2010-04-01 10:36:26 +0200

    Refactor Evaluator

--- src/xhl/core/Evaluator.java
+++ src/xhl/core/Evaluator.java
@@ -2,6 +2,11 @@ package xhl.core;
 
 import xhl.core.exceptions.*;
 
+/**
+ * XHL code evaluator.
+ *
+ * @author Sergej Chodarev
+ */
 public class Evaluator {
     private final SymbolTable symbolTable = new SymbolTable();
 
@@ -9,6 +14,14 @@ public class Evaluator {
         symbolTable.putAll(module.getSymbols());
     }
 
+    /**
+     * Evaluate language object.
+     *
+     * @param obj
+     *            object to evaluate
+     * @return result of evaluation
+     * @throws EvaluationException
+     */
     public Object eval(Object obj) throws EvaluationException {
         if (obj instanceof Symbol) {
             Symbol sym = (Symbol) obj;
@@ -17,19 +30,30 @@ public class Evaluator {
             else
                 throw new SymbolNotDefinedException(sym);
         } else if (obj instanceof CodeList) {
-            CodeList list = (CodeList) obj;
-            Object head = list.head();
-            if (!(head instanceof Symbol))
-                throw new HeadIsNotSymbolException();
-            Object func = eval(head);
-            if (!(func instanceof Executable))
-                throw new FunctionUndefinedException((Symbol) head);
-            return ((Executable) func).exec(list.tail());
+            return evalList((CodeList) obj);
         } else {
             return obj;
         }
     }
 
+    private Object evalList(CodeList list) throws EvaluationException {
+        Object head = list.head();
+        if (!(head instanceof Symbol))
+            throw new HeadIsNotSymbolException();
+        Object func = eval(head);
+        if (!(func instanceof Executable))
+            throw new FunctionUndefinedException((Symbol) head);
+        return ((Executable) func).exec(list.tail());
+    }
+
+    /**
+     * Evaluate list of program expressions
+     *
+     * @param exprs
+     *            list containing program expressions
+     * @return result of evaluation
+     * @throws EvaluationException
+     */
     public Object evalAll(CodeList exprs) throws EvaluationException {
         Object result = null;
         for (Object expr : exprs) {
@@ -38,11 +62,23 @@ public class Evaluator {
         return result;
     }
 
-    public void putSymbol(Symbol sym, Object value) {
-        symbolTable.put(sym, value);
+    /**
+     * Put symbol into symbol table.
+     *
+     * @param symbol
+     * @param value
+     */
+    public void putSymbol(Symbol symbol, Object value) {
+        symbolTable.put(symbol, value);
     }
 
-    public Object getSymbol(Symbol sym) {
-        return symbolTable.get(sym);
+    /**
+     * Get value of symbol from symbol table.
+     *
+     * @param symbol
+     * @return value associated with symbol
+     */
+    public Object getSymbol(Symbol symbol) {
+        return symbolTable.get(symbol);
     }
 }
