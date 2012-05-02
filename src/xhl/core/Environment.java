f01b852db4e47261928230d2e8520ccf4d65f040
Sergej Chodarev <sergejx@centrum.sk>
2012-05-02 17:45:30 +0200

    Better support for local namespaces in Environment

--- src/xhl/core/Environment.java
+++ src/xhl/core/Environment.java
@@ -1,39 +1,43 @@
 package xhl.core;
 
+import java.util.Deque;
 import java.util.Map;
 import java.util.Set;
 
 import xhl.core.elements.Symbol;
 
+import static com.google.common.collect.Lists.newLinkedList;
 import static com.google.common.collect.Maps.newHashMap;
+import static com.google.common.collect.Sets.newHashSet;
 
 public class Environment<T> {
-    private final Map<Symbol, T> table = newHashMap();
-    private Environment<T> parent;
+    private Map<Symbol, T> table;
+    private final Deque<Map<Symbol, T>> stack = newLinkedList();
 
     public Environment() {
-    }
-
-    public Environment(Environment<T> parent) {
-        this.parent = parent;
+        push(); // Global namespace
     }
 
     public boolean containsKey(Symbol sym) {
-        return table.containsKey(sym)
-                || ((parent != null) && parent.containsKey(sym));
+        for (Map<Symbol, T> tbl : stack)
+            if (tbl.containsKey(sym))
+                return true;
+        return false;
     }
 
     public T get(Symbol sym) {
-        if (table.containsKey(sym))
-            return table.get(sym);
-        else if (parent != null)
-            return parent.get(sym);
-        else
-            return null;
+        for (Map<Symbol, T> tbl : stack)
+            if (tbl.containsKey(sym))
+                return tbl.get(sym);
+        return null;
     }
 
     public T put(Symbol sym, T value) {
         return table.put(sym, value);
+    }
+
+    public T putGlobal(Symbol sym, T value) {
+        return stack.getLast().put(sym, value);
     }
 
     public void putAll(Environment<T> t) {
@@ -43,6 +47,25 @@ public class Environment<T> {
     }
 
     public Set<Symbol> keySet() {
-        return table.keySet();
+        Set<Symbol> keys = newHashSet();
+        for (Map<Symbol, T> tbl : stack)
+            keys.addAll(tbl.keySet());
+        return keys;
+    }
+
+    /**
+     * Push a new local environment on the stack.
+     */
+    public void push() {
+        table = newHashMap();
+        stack.push(table);
+    }
+
+    /**
+     * Remove local environment.
+     */
+    public void pop() {
+        stack.removeFirst();
+        table = stack.peek();
     }
 }
