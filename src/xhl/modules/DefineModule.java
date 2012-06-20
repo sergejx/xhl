3d5f382a1e852a72f02fa2157d28671ef6a31e51
Sergej Chodarev <sergej.chodarev@tuke.sk>
2012-06-20 12:12:20 +0200

    DefineModule: Implement check function for setting a type of defined element

--- src/xhl/modules/DefineModule.java
+++ src/xhl/modules/DefineModule.java
@@ -1,8 +1,17 @@
 package xhl.modules;
 
+import com.google.common.collect.ImmutableMap;
 import xhl.core.EvaluationException;
 import xhl.core.GenericModule;
+import xhl.core.elements.Expression;
 import xhl.core.elements.Symbol;
+import xhl.core.validator.Type;
+import xhl.core.validator.ValidationResult;
+import xhl.core.validator.Validator;
+
+import java.util.ArrayList;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 public class DefineModule extends GenericModule {
 
@@ -13,5 +22,14 @@ public class DefineModule extends GenericModule {
             throw new EvaluationException(String.format(
                     "Symbol '%s' is already defined", symbol));
         evaluator.putSymbol(symbol, value);
+    }
+
+    @Check(name="define")
+    public ValidationResult checkDefine(Validator validator,
+                                        Symbol symbol,
+                                        Expression value) {
+        Type type = validator.check(value);
+        return new ValidationResult(Type.Null, null,
+                ImmutableMap.of(symbol, type));
     }
 }
